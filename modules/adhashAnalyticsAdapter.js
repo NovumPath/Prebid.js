f4a1366f0e13858b682828e18aa6f550b6a12cde
stefan <Lyserg_Diethel@protonmail.com>
2022-07-06 16:49:40 +0300

    PUB-169
    Added support for timeout field.
    Enabled bidder endpoint data-sending.
    Simplified logic determining winning bids.

--- modules/adhashAnalyticsAdapter.js
+++ modules/adhashAnalyticsAdapter.js
@@ -7,6 +7,7 @@ import { config } from '../src/config.js';
 const analyticsType = 'endpoint';
 const defaultUrl = 'https://hookb.in/wNgPLbjWkxtqWVaqDErX';
 var auctionTracker = {};
+var bidTimeouts = [];
 var auctionEndStorage = null;
 var platformURL;
 var bidderAnalyticsEndpoint;
@@ -43,38 +44,40 @@ const bidWon = function(eventType, args) {
   console.log(eventType, args);
   var relevantBidsData = [];
   var responses = auctionTracker[args.auctionId][args.adUnitCode].res;
-  var nonResponses = auctionTracker[args.auctionId][args.adUnitCode].nob;
+  var nonResponses = auctionTracker[args.auctionId][args.adUnitCode].nob; 
   var bidResponders = Object.keys(responses);
   var noBidResponders = Object.keys(nonResponses);
 
-  var bidResponsesRaw = auctionTracker[args.auctionId][args.adUnitCode].res //Get just the ones for this one adUnit
-  for(var bidder of bidResponders) {
-    if (args.bidderCode === bidder) {
-      //Mark bid as winner;
-      var winningBid = responses[bidder];
-      var winningBidData = auctionEndStorage.bidsReceived.filter(
-        bid => bid.bidderCode === args.bidderCode && bid.adUnitCode === args.adUnitCode
-      )[0]
+  var bidResponsesRaw = auctionTracker[args.auctionId][args.adUnitCode].res
+  var winningBid = {};
+  var winningBidData = auctionEndStorage.bidsReceived.filter(
+    bid => bid.bidderCode === args.bidderCode && bid.adUnitCode === args.adUnitCode
+  )[0]
 
-      winningBid.adTagId = args.adUnitCode;
-      winningBid.bid = true;
-      winningBid.language = window.navigator.language || '';
-      winningBid.userAgent = window.navigator.userAgent || '';
-      if(navigator.userAgentData && navigator.userAgentData.platform) {
-        winningBid.platform = navigator.userAgentData.platform;
-      } else {
-        winningBid.platform = navigator.platform;
-      }
-      winningBid.timeZone = new Date().getTimezoneOffset() / 60,
-      winningBid.width = winningBidData.width;
-      winningBid.height = winningBidData.height;
-      winningBid.screenWidth = screen.width;
-      winningBid.screenHeight = screen.height;
-      winningBid.size = `${winningBidData.width}x${winningBidData.height}`;
-      winningBid.win = true;
-      relevantBidsData.push(winningBid);
-    }
+  winningBid.adTagId = args.adUnitCode;
+  winningBid.bid = true;
+  winningBid.language = window.navigator.language || '';
+  winningBid.userAgent = window.navigator.userAgent || '';
+  if (navigator.userAgentData && navigator.userAgentData.platform) {
+    winningBid.platform = navigator.userAgentData.platform;
+  } else {
+    winningBid.platform = navigator.platform;
   }
+  winningBid.timeZone = new Date().getTimezoneOffset() / 60,
+    winningBid.width = winningBidData.width;
+  winningBid.height = winningBidData.height;
+  winningBid.screenWidth = screen.width;
+  winningBid.screenHeight = screen.height;
+  winningBid.size = `${winningBidData.width}x${winningBidData.height}`;
+  winningBid.win = true;
+
+  winningBid.cost = args.cpm / 1000;
+  winningBid.currency = args.currency;
+  winningBid.delay = args.timeToRespond;
+  winningBid.pageURL = auctionTracker.pageURL;
+  winningBid.ssp = args.bidder;
+
+  relevantBidsData.push(winningBid);
   for (var bidder of bidResponders) {
     if (bidResponsesRaw[bidder].ssp !== winningBid.ssp) {
       relevantBidsData.push(bidResponsesRaw[bidder]);
@@ -84,46 +87,53 @@ const bidWon = function(eventType, args) {
   for (var bidder of noBidResponders) {
     relevantBidsData.push(nonResponses[bidder]);
   }
-  //Send the JSON-stringified array to server
 
+  //Send the JSON-stringified array to server
   var payload = JSON.stringify(relevantBidsData);
   var bidderPayload;
-  var fullPlatformURL = platformURL + 'data.php?type=pbstats';
-  console.log('sending to ' + fullPlatformURL);
+  var platformUrlMatch = platformURL.match(/.+(?=protocol\.php)/)
+  var fullPlatformURL = (platformUrlMatch ? platformUrlMatch[0] : platformURL) + 'data.php?type=pbstats';
 
   ajax(fullPlatformURL, null, payload);
-  // if(bidderAnalyticsEndpoint && publisherId) {
-  //   bidderPayload = JSON.stringify(
-  //     {
-  //       platform: publisherId,
-  //       data: relevantBidsData
-  //     }
-  //   );
-  //   ajax(bidderAnalyticsEndpoint, null, bidderPayload);
-  // }
+  if(bidderAnalyticsEndpoint && publisherId) {
+    bidderPayload = JSON.stringify(
+      {
+        platform: publisherId,
+        data: relevantBidsData
+      }
+    );
+    ajax(bidderAnalyticsEndpoint, null, bidderPayload);
+  }
   console.log('relevantBidsData: ', relevantBidsData);
 }
 
 const bidTimeout = function(eventType, args) {
+  bidTimeouts = args;
   console.log(eventType, args);
 }
 
 const auctionEnd = function(eventType, args) {
   console.log(eventType, args);
   auctionEndStorage = args;
-
+  //adding pageURL here:
+  if (!auctionTracker.pageURL) {
+    var firstRequest = args.bidderRequests[0];
+    auctionTracker.pageURL = firstRequest.refererInfo.stack[firstRequest.refererInfo.stack.length - 1];
+  }
+  
   // Populate Request info
   args.bidderRequests.forEach(req => {
-    for(var bid of req.bids){
-        auctionTracker[req.auctionId][bid.adUnitCode].req[req.bidderCode] = {
-            ssp: req.bidderCode,
-            pageURL: req.refererInfo.stack[req.refererInfo.stack.length - 1],
-            delay: null,
-            bid: false,
-            win: false,
-            cost: null
-        }
-     }
+    for (var bid of req.bids) {
+      auctionTracker[req.auctionId][bid.adUnitCode].req[req.bidderCode] = {
+        ssp: req.bidderCode,
+        pageURL: auctionTracker.pageURL,
+        delay: null,
+        bid: false,
+        win: false,
+        timeout: false,
+        cost: null
+      }
+    }
   })
 
   // Populate Response info
@@ -136,6 +146,7 @@ const auctionEnd = function(eventType, args) {
           delay: res.timeToRespond,
           bid: true,
           win: false,
+          timeout: false,
           cost: res.cpm / 1000,
           currency: res.currency
       }
@@ -151,15 +162,45 @@ const auctionEnd = function(eventType, args) {
         delay: null,
         bid: false,
         win: false,
+        timeout: false,
         cost: 0.0,
     }
   })
-
+  
+  bidTimeouts.forEach(req => {
+    var unitAuction = auctionTracker[req.auctionId][req.adUnitCode];
+      var noBidObject = unitAuction.nob;
+      if(!noBidObject[req.bidder]) {
+        noBidObject[req.bidder] = {
+          ssp: req.bidder,
+          pageURL: auctionTracker.pageURL,
+          bid: false,
+          win: false,
+          timeout: true,
+          cost: 0.0,
+        }
+      } else {
+        // Do we want this to be true if it's already set, possibly by the explicit noBid event?
+        noBidObject[req.bidder].timeout = true; 
+      }
+})
   console.log('Auction Tracker:', auctionTracker);
 }
 
 const noBid = function(eventType, args) {
   console.log(eventType, args);
+  var auctionId = args.auctionId;
+  var adUnitCode = args.adUnitCode;
+  var bidder = args.bidder;
+  auctionTracker[auctionId][adUnitCode].nob[bidder] = {
+    bid: false,
+    cost: 0,
+    pageURL: auctionTracker.pageURL,
+    ssp: bidder,
+    timeout: false,
+    win: false
+  }
+
 }
 
 
